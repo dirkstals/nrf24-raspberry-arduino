2ef72a36fa154cd886f0b5e7c4a46496e79e73e5
Dirk <dirkstals@gmail.com>
2018-02-08 13:46:24 +0100

    change address

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var irqPin = 25;
 var nrf = require('nrf');
 var radio = nrf.connect(spiDev, cePin); // Connect to the radio
 radio.channel(0x4c).dataRate('1Mbps').crcBytes(2).autoRetransmit({count:15, delay:4000});
-radio._debug = true;
+
 // Start the radio
 radio.begin(function() {
 
 	var rx = radio.openPipe('rx', 0xF0F0F0F0E1);
-	var tx = radio.openPipe('tx', 0xF0F0F0F0F2); // Send to address
+	var tx = radio.openPipe('tx', 0xF0F0F0F0D2); // Send to address
   var degrees = 0;
 
   // Fires when our transmission pipe is ready
