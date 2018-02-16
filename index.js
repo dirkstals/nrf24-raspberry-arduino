c996510023ab7aed0b12209635907fd0e4b0b211
Dirk <dirkstals@gmail.com>
2018-02-16 20:37:27 +0100

    with switch

--- index.js
+++ index.js
@@ -2,31 +2,37 @@ var spiDev = "/dev/spidev0.0";
 var cePin = 22;
 var irqPin = 25;
 
+var configPin = 7;
+
+
+
 var nrf = require('nrf');
 var radio = nrf.connect(spiDev, cePin, irqPin); // Connect to the radio
-radio.channel(0x4c).dataRate('1Mbps').crcBytes(2).autoRetransmit({count:15, delay:4000});
+// radio.channel(0x4c).dataRate('1Mbps').crcBytes(2).autoRetransmit({count:15, delay:4000});
 
 // Start the radio
 radio.begin(function() {
 
 	// var rx = radio.openPipe('rx', 0xF0F0F0F0E1);
-	var tx = radio.openPipe('tx', 0xF0F0F0F0D2); // Send to address
+	var tx = radio.openPipe('tx', 0xE8E8F0F0E1); // Send to address
   var degrees = 0;
 
   // Fires when our transmission pipe is ready
 	tx.on('ready', function() {
-		console.log("TX Ready");
-		setInterval(function(){
-        degrees += 30;
-        tx.write(degrees.toString()); // Send a message
-        console.log(degrees);
-        if(degrees > 180) {
-          degrees = 0;
-        }
-    }, 1000);
-	});
 
-	// rx.pipe(tx);
+		wpi.setup('wpi');
+		var started = false;
+		var clock = null;
+
+		wpi.pinMode(configPin, wpi.INPUT);
+		wpi.pullUpDnControl(configPin, wpi.PUD_UP);
+		wpi.wiringPiISR(configPin, wpi.INT_EDGE_BOTH, function() {
+		  var button = wpi.digitalRead(configPin);
+			if(button) {
+				tx.write('hello world');
+			}
+		});
+	});
 
 	// Handler for errors
 	tx.on('error', function(e) {
